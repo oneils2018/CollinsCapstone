d7635030c458861431fcc70013b84f6e31426022
Meshak091011 <michaeleshak2@gmail.com>
2022-02-25 14:12:42 -0500

    Documentation for Variables.py

--- Variables.py
+++ Variables.py
@@ -1,40 +1,44 @@
-##### FILTER Variables #####
+"""
 
-# The time_range is how far back in time and in the future of the current index does the program check for hits
-# This has the largest hit on performance (Exponentially).
+Description:
+    The variables down below are used for filters
+
+    The time_range is how far back in the past and in the future of the current index does
+    the program check for hits. It effects the performance the most.
+
+    Positive represent the minimum number of neighbor hitts that will be attained for the hit to be added to the plot.
+
+    max_distance represent the maximum distance in a 3d space. Z is time that a hit must be under to be added to the plot.
+
+    Increment represent the duration of time stamps the program will process at a time then plot to a graph.
+    The event number after the increment from which the filter starts the number of events filtered into a single frame
+
+"""
+
+"""
+
+Description:
+   Tracking variables:
+        samples_size checks how large a sample size should be by averaging pixels to find the center of an object
+        
+        min_distance is the origin for the number of misses in a row before we "give up" and use the last hit as a bound
+        
+        min_hits is the minimum number of hits that is within a sample required to identify it as an object and draw the box 
+        
+        tolerance represent hos big should the box be 
+        
+        max_tacking_iterations is how many times should we iterate the tracking function before we give up
+    
+"""
+
 time_range = 7
-
-# Positive is the minimum number of neighbor hits that must be attained for the hit to be added to the plot.
 positive = 2
-
-# The max_distance is the maximum distance in a 3d space
-# Where z is time that a hit must be under to be added to the plot.
 max_distance = 10
-
-# The increment is the duration of time stamps the program will process at a time then plot to a graph.
 increment = 10000
-
-# The event number after the increment from which the filter starts
 min = 0
-
-# The number of events filtered into a single frame
 max = 40000
-
-
-
-##### TRACKING Variables #####
-
-# Determines how large a sample size we should take for averaging pixels to find the center of an object
 sample_size = 40
-
-# Threshold for number of misses in a row before we "give up" and use the last hit as a bound
 min_distance = 10
-
-# Minimum number of hits within a sample required to identify it as an object and draw a box
 min_hits = 50
-
-# How much bigger should the box be then the object
 tolerance = 5
-
-# How many times should we iterate the tracking function before we give up
 max_tracking_iterations = 50
